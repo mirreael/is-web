87c8bacc15fd7637d9be1f227207e07527b9509e
Amalia Tereshenko <144378797+mirreael@users.noreply.github.com>
2024-12-19 15:57:12 +0300

    feat: improve conversationScript.js

--- scripts/conversationScript.js
+++ scripts/conversationScript.js
@@ -2,46 +2,47 @@ document.addEventListener('DOMContentLoaded', () => {
     const messagesContainer = document.getElementById('messagesContainer');
     const preloader = document.getElementById('preloader');
     const messageTemplate = document.getElementById('messageTemplate').content;
+    const URI = 'https://jsonplaceholder.typicode.com/posts';
+
     let messages = [];
-    let currentIndex = 0;
+
 
     async function loadMessages() {
         try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-            if (!response.ok) throw new Error('Ошибка загрузки данных');
-
+            const response = await fetchData(URI);
             messages = await response.json();
             preloader.style.display = 'none';
             messagesContainer.innerHTML = '';
 
-            loadNextBatch();
+            messages.forEach(createMessage);
         } catch (error) {
-            preloader.style.display = 'none';
-            messagesContainer.innerHTML = '<div class="error">Что-то пошло не так</div>';
+            handleError(error);
         }
     }
 
-    function loadNextBatch() {
-        const nextBatch = messages.slice(currentIndex, currentIndex + 10);
-        nextBatch.forEach((message) => {
-            const messageуElement = document.importNode(messageTemplate, true);
-            messageElement.querySelector('.conversation__message-title').textContent = message.title;
-            messageElement.querySelector('.conversation__message-body').textContent = message.body;
-            messagesContainer.appendChild(messageElement);
-        });
-        currentIndex += 10;
-    }
-
-    function handleScroll() {
-        const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
-        if (scrollTop + clientHeight >= scrollHeight - 5 && currentIndex < messages.length) {
-            loadNextBatch();
+    async function fetchData(URI) {
+        const response = await fetch(URI);
+        if (!response.ok) {
+            throw new Error('Ошибка загрузки данных');
         }
+        return response;
     }
 
 
-    window.addEventListener('scroll', handleScroll);
+    function createMessage(message) {
+        const messageElement = document.importNode(messageTemplate, true);
+        messageElement.querySelector('.conversation__message-title').textContent = message.title;
+        messageElement.querySelector('.conversation__message-body').textContent = message.body;
+        messagesContainer.appendChild(messageElement);
+    }
 
-    //TO-DO: improve exception
-    loadMessages()
+
+    function handleError(error) {
+        preloader.style.display = 'none';
+        messagesContainer.innerHTML =
+            '<div class="error">Что-то пошло не так</div>';
+        alert(error.message);
+    }
+
+    loadMessages();
 });
